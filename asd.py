10a1e128863db8bc7ab88d018ee51c5c743de214
kavin413 <srisudhakavin@gmail.com>
2025-11-27 16:46:46 +0530

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,6 +1,3 @@
-class StringReverser:
-    def reverse_words(self, input_string: str) -> str:
-        
-        words = input_string.split()  
-        reversed_words = words[::-1]  
-        return " ".join(reversed_words) 
+matrix = [[1, 2], [3, 4], [5, 6]]
+flat_list = [item for sublist in matrix for item in sublist]
+print(flat_list)
